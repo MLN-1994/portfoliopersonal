ce09f2ee3cd4c1033233a3b92ea4334dbf16b674
Mariano <marianolangge@gmail.com>
2023-02-17 16:00:16 -0300

    responsive styles skills & title

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,31 +2,32 @@ import React, { useEffect, useState } from "react";
 import dataSkills from "../data/dataSkills.json";
 
 function Skills() {
-
   const [datos, setDatos] = useState([]);
 
   console.log(datos);
 
   useEffect(() => {
-
     setDatos(dataSkills);
-
   }, []);
 
   return (
     <>
-    <div className="flex flex-nowrap">
-
-      {datos.map((dato, index) => (
-
-        <div className="border" key={index}>
-          <p>{dato.name}</p>
-          <img src={dato.image} alt="" />
-        </div>
-
-      ))}
-
-    </div>
+    <div className="flex justify-center text-3xl px-2 py-2">Skills</div>
+    
+      <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-4  xl:grid-cols-7 gap-2 item-center p-2 m-2 ">
+        {datos.map((dato, index) => (
+          <div className=" backdrop-filter bg-neutral-50 drop-shadow-2xl  opacity-70 rounded  hover:opacity-100 shadow-xl w-42 h-42  flex justify-center items-center p-4 transition ease-in 800" key={index}>
+            <div className=" ">
+              
+              <img className="w-16 mb-4 " src={dato.image} alt="" />
+              <div className="flex justify-center">
+                <p className="text-black absolute bottom-0 font-bold opacity-70">{dato.name}</p>
+                </div>
+              
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -6,10 +6,10 @@ export default function Title() {
     <div className="flex justify-center">
     <div className="flex flex-col">
         <div className="">
-            <p className="text-7xl">Mariano Langge</p>
+            <p className="text-4xl sm:text-6xl xl:text-8xl">Mariano Langge</p>
             </div>
         <div className="">
-          <p className="text-white opacity-60 text-xl">Frontend Developer</p>
+          <p className="text-white opacity-60 sm:text-3xl xl:text-4xl">Frontend Developer</p>
         </div>
       </div>
       </div>
