d8b4e189910661714489a2a3b3279c1b6f61053d
Mariano <marianolangge@gmail.com>
2023-03-15 17:42:54 -0300

    fix

--- src/components/socialNetworks/SocialNetworks.jsx
+++ src/components/socialNetworks/SocialNetworks.jsx
@@ -5,12 +5,12 @@ export default function SocialNetworks() {
   return (
     <div className="grid grid-cols-12 my-4">
       <div className="col-start-10 ">
-        <a target="_blank" href="https://github.com/" className=" flex justify-center items-center ">
+        <a target="_blank" rel="noreferrer" href="https://github.com/" className=" flex justify-center items-center ">
           <BsGithub className="text-5xl"/>
         </a>
       </div>
       <div className="">
-        <a target="_blank" href="https://www.linkedin.com/in/mariano-langge-46503b235/" className="	flex justify-center items-center">
+        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/mariano-langge-46503b235/" className="	flex justify-center items-center">
           <BsLinkedin className="text-5xl" />
         </a>
       </div>
