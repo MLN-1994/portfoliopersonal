2a7df5878defa41fae5d09165c4fa551dcd09dd7
Mariano <marianolangge@gmail.com>
2023-03-15 19:48:19 -0300

    ..

--- src/components/personalinfo/PersonalInfo.jsx
+++ src/components/personalinfo/PersonalInfo.jsx
@@ -3,23 +3,21 @@ import React from "react";
 export default function PersonalInfo() {
   return (
     <>
-      <div className="flex justify-center my-6 ">
+      <div className="flex justify-center my-6  ">
         <div className="p-4 w-2/3 mx-auto overflow-auto backdrop-filter  drop-shadow-4xl text-black  opacity-70 rounded  hover:opacity-100 shadow-xl">
           <div className="p-2">
-            <p className="p-4  whitespace-pre-line font-semibold text-center leading-[1.7]">
-            Soy un desarrollador frontend junior de 28 años apasionado por crear
-            interfaces de usuario atractivas y funcionales. Me encanta aprender
-            nuevas tecnologías y experimentar con diseños innovadores para
-            mejorar la experiencia del usuario. Mi experiencia incluye el
-            desarrollo de aplicaciones web utilizando HTML, CSS, JavaScript y
-            frameworks como React. Además, tengo habilidades en la resolución de
-            problemas, trabajo en equipo y comunicación efectiva. Siempre estoy
-            buscando oportunidades para crecer y mejorar mis habilidades en el
-            campo del desarrollo frontend. Otros de mis pasatiempos son jugar al
-            ajedrez y tocar la guitarra.
-          </p>
+            <p className="p-4  whitespace-pre-line font-semibold text-center leading-[1.7]  ">
+              Soy un desarrollador frontend junior apasionado por crear
+              interfaces de usuario atractivas y funcionales. Me gusta aprender
+              nuevas tecnologías y experimentar con diseños innovadores para
+              mejorar la experiencia del usuario. Tengo experiencia en el
+              desarrollo de aplicaciones web utilizando HTML, CSS, JavaScript y
+              React. También tengo habilidades en la resolución de problemas,
+              trabajo en equipo y comunicación efectiva. Estoy buscando
+              oportunidades para crecer y mejorar mis habilidades en el campo
+              del desarrollo frontend.
+            </p>
           </div>
-          
         </div>
       </div>
     </>
